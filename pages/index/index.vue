e6a5894134a43ffae0d4621696adbfa84d31912c
junlin.liu <763606865@qq.com>
2021-07-12 19:07:22 +0800

    首页banners

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,24 +1,43 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+		<!-- 头部轮播 -->
+		<swiper class="swiper" circular :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+			<swiper-item v-for="(item,index) in banners" :key="index">
+				<view class="swiper-item uni-bg-red">
+					<img :src="item.path" alt="">
+				</view>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
 <script>
+	import swiper from './swiper.vue'
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title:'首页',
+				banners: [],
+				indicatorDots: true,
+				autoplay: true,
+				interval: 4000,
+				duration: 500
 			}
 		},
 		onLoad() {
-
+			uni.request({
+			    url: 'http://www.oldbaby.com/api',
+				method: 'post',
+			    success: (res) => {
+			        this.banners = res.data.data.banners;
+			    }
+			});
 		},
 		methods: {
-
+			
+		},
+		components: {
+			swiper,
 		}
 	}
 </script>
@@ -48,5 +67,39 @@
 	.title {
 		font-size: 36rpx;
 		color: #8f8f94;
+	}
+	.uni-margin-wrap {
+		width:690rpx;
+		width: 100%;;
+	}
+	.swiper {
+		width:100%;
+		height: 300rpx;
+	}
+	.swiper-item {
+		display: block;
+		height: 300rpx;
+		line-height: 300rpx;
+		text-align: center;
+	}
+	
+	.swiper-list {
+		margin-top: 40rpx;
+		margin-bottom: 0;
+	}
+	
+	.uni-common-mt{
+		margin-top:60rpx;
+		position:relative;
+	}
+	
+	.info {
+		position: absolute;
+		right:20rpx;
+	}
+	
+	.uni-padding-wrap {
+	    width:550rpx;
+	    padding:0 100rpx;
 	}
 </style>
